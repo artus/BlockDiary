a84df5d1bdd137c5f1cabde84c9ab534b29665db
artusvranken <artusvranken@gmail.com>
2018-04-12 15:36:09 +0200

    Added a scope to the login request.

--- js/BlockDiary.js
+++ js/BlockDiary.js
@@ -18,7 +18,7 @@ var app = new Vue({
             }
         },
         loginClicked() {
-            this.blockstack.redirectToSignIn("https://artusvranken.github.io/BlockDiary/", "https://artusvranken.github.io/BlockDiary/manifest.json", ['store_write']);
+            this.blockstack.redirectToSignIn("https://artusvranken.github.io/BlockDiary/", "https://artusvranken.github.io/BlockDiary/manifest.json", ['store_write', 'publish_data']);
         },
         logoutClicked() {
             this.blockstack.signUserOut(window.location.href);
