b53a914331eaaab4fa8b88940ab86b1c829eef8a
artusvranken <artusvranken@gmail.com>
2018-04-17 11:09:18 +0200

    Rewrote the debounce function.

--- js/BlockDiary.js
+++ js/BlockDiary.js
@@ -28,26 +28,16 @@ var app = new Vue({
             this.blockstack.signUserOut(window.location.href);
         },
         loadData() {
+            console.log("Loading data...");
             this.blockstack.getFile('blockdiary.json', { decrypt: true }).then(file => this.content = JSON.parse(file).content).catch(error => console.log(error));
         },
         saveData() {
+            console.log("Saving data...");
             this.blockstack.putFile('blockdiary.json', JSON.stringify({ content: this.content }), { encrypt: true }).then(() => console.log("Diary saved.")).catch(error => console.log(error));
         },
-        handleTextUpdate(e) {
-            if (this.blockstack.isUserSignedIn()) {
-                this.typing = true;
-                setTimeout(() => { this.typing = false; }, 500);
-                setTimeout(() => { this.handleSave(); }, 1000);
-            }
-        },
-        handleSave() {
-            if (!this.typing) {
-                this.saving = true;
-                console.log("saving...");
-                this.saveData();
-                this.saving = false;
-            }
-        },
+        onTextUpdate() {
+            _.debounce(saveData, 500);
+        }
     }
 });
 
